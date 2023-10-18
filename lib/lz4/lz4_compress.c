c8c17c6c31636adc35775fef166fa91297887526
J0SH1X <aljoshua.hell@gmail.com>
2023-10-18 13:52:49 +0200

    LZ4: fix merge errors

--- lib/lz4/lz4_compress.c
+++ lib/lz4/lz4_compress.c
@@ -506,7 +506,6 @@ int LZ4_compress_fast(const char *source, char *dest, int inputSize,
 	return LZ4_compress_fast_extState(wrkmem, source, dest, inputSize,
 		maxOutputSize, acceleration);
 }
-EXPORT_SYMBOL(LZ4_compress_fast);
 
 int LZ4_compress_default(const char *source, char *dest, int inputSize,
 	int maxOutputSize, void *wrkmem)
@@ -762,7 +761,6 @@ int LZ4_compress_destSize(
 	return LZ4_compress_destSize_extState(wrkmem, src, dst, srcSizePtr,
 		targetDstSize);
 }
-EXPORT_SYMBOL(LZ4_compress_destSize);
 
 /*-******************************
  *	Streaming functions
@@ -934,7 +932,6 @@ int LZ4_compress_fast_continue(LZ4_stream_t *LZ4_stream, const char *source,
 		return result;
 	}
 }
-EXPORT_SYMBOL(LZ4_compress_fast_continue);
 
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_DESCRIPTION("LZ4 compressor");
